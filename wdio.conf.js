3b4a4cb19118c8a7fa2ce4acfff01362d05f0ca6
Mihail Parvanov <mihail.parvanov@progress.com>
2020-10-27 10:28:00 +0200

    chore: enable google headless and max instances

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,13 +51,13 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 1,
+        maxInstances: 2,
         //
         browserName: 'chrome',
         'goog:chromeOptions': {
             // to run chrome headless the following flags are required
             // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-            //args: ['--headless', '--disable-gpu'],
+            args: ['--headless', '--disable-gpu'],
             },
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
