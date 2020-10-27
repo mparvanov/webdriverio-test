329358f25d2e380da37b2335adaf55e7af6013bf
Mihail Parvanov <mihail.parvanov@progress.com>
2020-10-27 10:41:02 +0200

    chore: uncomment the default sample tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,8 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        // './test/specs/**/*.js'
+        //'./test/specs/**/*.js'
+        
         './test/specs/**/kendo*.js'
     ],
     // Patterns to exclude.
